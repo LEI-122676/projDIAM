235986c8fa4ee2ceea9a2068f017a7db94c7c34c
tigago <tiago.freitas.devs@gmail.com>
2026-05-07 16:47:35 +0100

    +urls & serializers

--- idjango/models.py
+++ idjango/models.py
@@ -11,12 +11,15 @@ class Evento(models.Model):
     data = models.DateField()
     descricao = models.TextField()
     participantes = models.ForeignKey(User, on_delete=models.CASCADE, related_name='eventos_inscritos')
+    capacidade_max = models.IntegerField()
 
 class Receita(models.Model):
     nome = models.CharField(max_length=50)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     instrucao = models.TextField()
     ingredientes = models.ManyToManyField(Ingrediente)
+    classificacao = models.FloatField()
+    comentarios = models
 
 class Frigorifico(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -26,3 +29,6 @@ class Utilizador(models.Model):
     utilizador = models.OneToOneField(User, on_delete=models.CASCADE)       # "extends"
     eventos = models.ManyToManyField(Evento, on_delte=models.DO_NOTHING)
     receitas = models.ManyToManyField(Receita, on_delete=models.DO_NOTHING)
+
+#class Comentario(models.Model):
+#    utilizador = models.
--- idjango/urls.py
+++ idjango/urls.py
@@ -2,4 +2,16 @@ from django.urls import path
 from . import views
 
 app_name = 'idjango'
-urlpatterns = []
+urlpatterns = [
+    path('api/utilizadores/', views.utilizadores),
+    path('api/utilizadores/<int:utilizador_id>', views.utilizador_detail),
+
+    path('api/receitas/', views.receitas),
+    path('api/receitas/<int:receita_id>', views.receita_detail),
+
+    path('api/eventos/', views.eventos),
+    path('api/eventos/<int:evento_id>', views.evento_detail),
+
+    path('api/ingredientes/', views.ingredientes),
+    path('api/ingredientes/<int:ingrediente_id>', views.ingrediente_detail),
+]
