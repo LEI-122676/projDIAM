3e1502f0056aac1fed593a81f847369ed99aedc9
122704 <pppns@iscte-iul.pt>
2026-05-07 16:28:37 +0100

    Imagens/Sidebar/Styles

--- ireact/src/components/sidebar.jsx
+++ ireact/src/components/sidebar.jsx
@@ -1,27 +1,31 @@
 import 'react';
 import '../css/styles.css';
 
+// Importação das imagens (ajuste o caminho conforme a sua pasta)
+import iconeReceitas from '../assets/receitas.svg';
+import iconeEventos from '../assets/calendario.svg';
+import iconePerfil from '../assets/perfil.svg';
+import iconeFrigorifico from '../assets/frigorifico.svg';
+
 const Sidebar = () => {
   return (
     <nav className="sidebar">
       <ul className="nav-list">
         <li className="nav-item">
-          <svg className="icon" viewBox="0 0 24 24">
-            <path />
-          </svg>
+          <img src={iconeReceitas} alt="Receitas" className="nav-icon-img" />
           <span>Receitas</span>
         </li>
         <li className="nav-item">
-          <svg className="icon" viewBox="0 0 24 24">
-            <path />
-          </svg>
+          <img src={iconeEventos} alt="Eventos" className="nav-icon-img" />
           <span>Eventos</span>
         </li>
         <li className="nav-item">
-          <svg className="icon" viewBox="0 0 24 24">
-            <path />
-          </svg>
+          <img src={iconeFrigorifico} alt="Frigorífico" className="nav-icon-img" />
           <span>Frigorífico</span>
+        </li>
+        <li className="nav-item">
+          <img src={iconePerfil} alt="Perfil" className="nav-icon-img" />
+          <span>Perfil</span>
         </li>
       </ul>
     </nav>
