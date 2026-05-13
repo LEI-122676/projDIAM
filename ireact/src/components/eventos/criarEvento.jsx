b5e621c421bbd632f43c47e80ab99ff788ed4193
LEI-122667 <sacrz@iscte-iul.pt>
2026-05-13 10:54:00 +0100

    Login, Registar, Ver Eevntos e Criar Evento

--- ireact/src/components/eventos/criarEvento.jsx
+++ ireact/src/components/eventos/criarEvento.jsx
@@ -1,12 +1,81 @@
-import React from 'react'; // Corrigido aqui
+import React, { useState, useEffect } from 'react';
 import Header from '../maincomponents/header.jsx';
 import Sidebar from '../maincomponents/sidebar.jsx';
-import { useNavigate } from 'react-router-dom';
-import '../../css/styles.css';
+import '../../css/styles.css'
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import PopupModal from '../maincomponents/PopupModal.jsx';
+
 
 const CriarEvento = () => {
+
+    const URL_CRIAR_EVENTO = 'http://localhost:8000/idjango/api/eventos/';
+
     const navigate = useNavigate();
 
+    const [nome, setNome] = useState('');
+    const [descricao, setDescricao] = useState('');
+
+    const [popupConfig, setPopupConfig] = useState({ isOpen: false, title: '', message: '', singleButton: true, onConfirm: () => {}, onCancel: () => {} });
+    const closePopup = () => setPopupConfig(prev => ({ ...prev, isOpen: false }));
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!nome) {
+            setPopupConfig({
+                isOpen: true,
+                title: 'Erro no nome',
+                message: 'Por favor, dê um nome ao evento.',
+                singleButton: true,
+                confirmText: 'Ok',
+                onConfirm: closePopup,
+                onCancel: closePopup
+            });
+            return;
+        }
+
+        if (descricao.length < 10) {
+            setPopupConfig({
+                isOpen: true,
+                title: 'Erro na descrição',
+                message: 'Por favor, dê uma descrição ao evento ( tamanho minimo de dez carateres).',
+                singleButton: true,
+                confirmText: 'Ok',
+                onConfirm: closePopup,
+                onCancel: closePopup
+            });
+            return;
+        }
+        
+        const utilizadorId = localStorage.getItem('utilizadorId');
+
+        const payload = {
+            nome: nome,
+            descricao: descricao,
+            criador: parseInt(utilizadorId, 10)
+        };
+
+
+        axios.post(URL_CRIAR_EVENTO, payload).then(res => {
+            setPopupConfig({
+                isOpen: true,
+                title: 'Sucesso',
+                message: 'Evento criado com sucesso.',
+                singleButton: true,
+                confirmText: 'Ok',
+                onConfirm: closePopup,
+                onCancel: closePopup
+            });navigate(-1);})
+            
+            .catch(err => {console.error(err);
+                if (err.response && err.response.data) {
+                    alert('Erro ao criar evento: ' + JSON.stringify(err.response.data));
+                } else {
+                    alert('Erro de conexão ao criar evento.');
+                }
+            });
+    };
 
     return (
         <div className="body-wrapper">
@@ -14,8 +83,57 @@ const CriarEvento = () => {
             <div className="main-wrapper">
                 <Sidebar />
                 <main className="content-profile">
+                    <h1 className="page-title-underline">Criar Evento</h1>
+                    <div className="create-recipe-container">
+                        <div className="recipe-form-section">
+                            <div className="form-group">
+                                <label>Nome*:</label>
+                                <input
+                                    type="text"
+                                    className="input-beige"
+                                    placeholder="Dê um nome ao seu evento"
+                                    value={nome}
+                                    onChange={(e) => setNome(e.target.value)}
+                                    style={{ color: 'black' }}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Descrição*:</label>
+                                <textarea
+                                    className="input-beige"
+                                    placeholder="Detalhes sobre o local, data e o que levar"
+                                    value={descricao}
+                                    onChange={(e) => setDescricao(e.target.value)}
+                                    style={{ 
+                                        color: 'black', 
+                                        height: '350px', 
+                                        padding: '20px', 
+                                        resize: 'none'
+                                    }}
+                                />
+                            </div>
+                        </div> 
+                        <div className="recipe-image-section">
+                            <div className="create-actions-group">
+                                <button className="btn-cancel" onClick={() => navigate(-1)} >Cancelar</button>
+                                <button className="btn-create-submit" onClick={handleSubmit}>Criar</button>
+                            </div>
+                        </div>
+
+                    </div>
                 </main>
             </div>
+            <PopupModal 
+                isOpen={popupConfig.isOpen}
+                title={popupConfig.title}
+                message={popupConfig.message}
+                singleButton={popupConfig.singleButton}
+                confirmText={popupConfig.confirmText || 'OK'}
+                cancelText={popupConfig.cancelText || ''}
+                onConfirm={popupConfig.onConfirm}
+                onCancel={popupConfig.onCancel}
+                />
         </div>
     );
 };
--- ireact/src/components/eventos/descobrirEventos.jsx
+++ ireact/src/components/eventos/descobrirEventos.jsx
@@ -1,4 +1,4 @@
-import React from 'react'; // Corrigido aqui
+import React from 'react';
 import Header from '../maincomponents/header.jsx';
 import Sidebar from '../maincomponents/sidebar.jsx';
 import iconeLupa from "../../assets/lupa.svg";
@@ -6,22 +6,67 @@ import iconeFiltro from "../../assets/filtro.svg";
 import iconeCalendario from "../../assets/calendario.svg";
 import { useNavigate } from 'react-router-dom';
 import '../../css/styles.css';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import PopupModal from '../maincomponents/PopupModal.jsx';
+
 
 const ExplorarEventos = () => {
+
+    const URL_EVENTOS = 'http://localhost:8000/idjango/api/eventos/';
+    const URL_USER = 'http://localhost:8000/idjango/api/user/';
+
     const navigate = useNavigate();
-    const eventosPlaceholder = [
-        "Evento A", "Evento B", "Evento C", "Evento D", "Evento E",
-        "Evento F", "Evento G", "Evento H", "Evento I", "Evento J",
-        "Evento K", "Evento L", "Evento M", "Evento N", "Evento O"
-    ];
-    const [searchTerm, setSearchTerm] = React.useState('');
+
+    const [searchQuery, setSearchQuery] = React.useState('');
+    const [eventos, setEventos] = React.useState([]);
+
+    const [userName, setUsername] = React.useState(null);
+
+    const [popupConfig, setPopupConfig] = useState({ isOpen: false, title: '', message: '', singleButton: true, onConfirm: () => {}, onCancel: () => {} });
+    const closePopup = () => setPopupConfig(prev => ({ ...prev, isOpen: false }));
+
+    useEffect(() => {
+        axios.get(URL_EVENTOS).then( response => setEventos(response.data))
+        .catch( () => console.log("unable to load events"));
+
+        axios.get(URL_USER, {withCredentials: true})
+            .then( response => setUsername(response.data.username))
+            .catch( () => console.log("user not logged in"));
+    
+    },[]);
 
     const handleSearch = (e) => {
         e.preventDefault();
-        if (searchTerm.trim()) {
-            navigate(`/eventos?search=${encodeURIComponent(searchTerm)}`);
+        if (searchQuery.trim()) {
+            navigate(`/eventos?search=${encodeURIComponent(searchQuery.trim())}`);
         }
     };
+
+    const eventosFiltrados = eventos.filter( evento =>{
+        const nomeEvento = evento.nome || "";
+        const matchesSearch = nomeEvento.toLowerCase().includes(searchQuery.toLowerCase());
+        return matchesSearch
+    })
+
+    const handleCriarEvento = (e) =>{
+        e.preventDefault();
+        if( userName){
+            navigate('/eventos/criarEvento')
+        }
+        else{
+            setPopupConfig({
+                isOpen: true,
+                title: 'Acesso Restrito',
+                message: 'Precisas de iniciar sessão para criar um novo evento. Crie uma conta ou faça login para criar o seu evento!',
+                singleButton: false,
+                confirmText: 'Iniciar Sessão',
+                onConfirm: () => navigate('/login'),
+                onCancel: closePopup
+            });
+        }
+    }
 
     return (
         <div className="body-wrapper">
@@ -31,52 +76,67 @@ const ExplorarEventos = () => {
                 <main className="content-profile">
                     {/* Alinhado à esquerda */}
                     <div className="profile-grid" style={{ margin: '0', maxWidth: '100%' }}>
-
-                        <h1 className="page-title-underline">Descobrir Eventos</h1>
-
+                        { userName ? <><h1 className="page-title-underline">Descobrir Eventos - {userName}</h1></>:
+                        <><h1 className="page-title-underline">Descobrir Eventos</h1></>}
                             <div className="recipes-action-bar">
                                 <div className="recipes-search-container">
-                                <form onSubmit={handleSearch} className="main-search-input event-search-box">
                                     <input
                                         type="text"
-                                        placeholder="Pesquisar eventos"
-                                        className="main-search-input"
-                                        value={searchTerm}
-                                        onChange={(e) => setSearchTerm(e.target.value)}
+                                        placeholder="Pesquisar eventos..."
+                                        className="main-search-input recipe-search-box"
+                                        value={searchQuery}
+                                        onChange={(e) => setSearchQuery(e.target.value)}
+                                        style={{ color: 'black' }}
                                     />
-                                    <button type="submit"  onClick={handleSearch} className="btn-search-bar">
-                                        <img src={iconeLupa} alt="Lupa" style={{ width: '20px', height: '20px' }} />
+                                    <img src={iconeLupa} alt="Lupa" className="recipe-icon-svg search-icon-pos" />
+                                </div>
+                                <div className="recipes-button-group">
+                                    <button className="btn-filter-fridge">
+                                        <img src={iconeFiltro} alt="Filtro" className="recipe-icon-svg" style={{marginRight: '8px'}} />
+                                        Calendario
+                                        <img src={iconeCalendario} alt="Calendario" className="recipe-icon-svg" style={{marginLeft: '8px'}} />
                                     </button>
-                                </form>
-                            </div>
 
-                            <div className="recipes-button-group">
-                                <button className="btn-filter-fridge">
-                                    <img src={iconeFiltro} alt="Filtro" className="recipe-icon-svg" style={{marginRight: '8px'}} />
-                                    Calendario
-                                    <img src={iconeCalendario} alt="Calendario" className="recipe-icon-svg" style={{marginLeft: '8px'}} />
-                                </button>
-
-                                <button onClick={() => navigate('/criarEvento')} className="btn-add-recipe">+</button>
+                                    <button onClick={handleCriarEvento} className="btn-add-recipe">+</button>
+                                </div>
                             </div>
                         </div>
 
                         <div className="recipes-grid">
-                            {eventosPlaceholder.map((name, index) => (
-                                <div key={index} className="recipe-card">
+                            {eventosFiltrados.map((evento, index) => (
+                                <div 
+                                    key={evento.id || index} 
+                                    className="recipe-card" 
+                                    onClick={() => navigate('eventos/verEvento', { state: { id: evento.id } })}
+                                    style={{ cursor: 'pointer' }}
+                                >
                                     <div className="recipe-image-placeholder">
                                         <span style={{ fontSize: '40px', color: '#D1CDBC' }}>✕</span>
                                     </div>
                                     <div className="recipe-card-footer">
-                                        <span className="ingredient-name">{name}</span>
+                                        <span className="ingredient-name">{evento.nome}</span>
                                     </div>
                                 </div>
                             ))}
+                            {eventosFiltrados.length === 0 && (
+                                <p style={{ gridColumn: '1 / -1', textAlign: 'center', marginTop: '20px' }}>
+                                    Nenhum evento encontrado com estes critérios.
+                                </p>
+                            )}
                         </div>
-                    </div>
-                </main>
+                    </main>
+                </div>
+                <PopupModal 
+                isOpen={popupConfig.isOpen}
+                title={popupConfig.title}
+                message={popupConfig.message}
+                singleButton={popupConfig.singleButton}
+                confirmText={popupConfig.confirmText || 'OK'}
+                cancelText={popupConfig.cancelText || 'Cancelar'}
+                onConfirm={popupConfig.onConfirm}
+                onCancel={popupConfig.onCancel}
+                />
             </div>
-        </div>
     );
 };
 
